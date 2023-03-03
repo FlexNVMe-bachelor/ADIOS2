16fac302cc98e87c7ca1d149bdafad6267ee9bb1
Albert Rise Nielsen <albert@risenielsen.dk>
2023-03-03 13:08:02 +0100

    Add ChunkNamingTests

---
+++ testing/adios2/transports/TestFileFlexNVMeChunkNaming.cpp
@@ -0,0 +1,47 @@
+#include "adios2/common/ADIOSTypes.h"
+#include "adios2/helper/adiosCommDummy.h"
+#include "adios2/toolkit/transport/file/FileFlexNVMe.h"
+#include "adios2/toolkit/transport/file/FilePOSIX.h"
+#include <gtest/gtest.h>
+#include <tuple>
+
+class ChunkNameTest
+: public ::testing::TestWithParam<
+      std::tuple<std::string, std::string, std::string, std::string>>
+{
+};
+
+TEST_P(ChunkNameTest, Names)
+{
+    const std::string &baseName = std::get<0>(GetParam());
+    const std::string &first = std::get<1>(GetParam());
+    const std::string &second = std::get<2>(GetParam());
+    const std::string &third = std::get<3>(GetParam());
+
+    adios2::transport::FileFlexNVMe e(adios2::helper::CommDummy());
+    e.Open(baseName, adios2::Mode::Undefined);
+    ASSERT_EQ(first, e.CreateChunkName());
+    ASSERT_EQ(second, e.CreateChunkName());
+    ASSERT_EQ(third, e.CreateChunkName());
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    NamingTests, ChunkNameTest,
+    ::testing::Values(std::make_tuple("basename", "basename#0", "basename#1",
+                                      "basename#2"),
+                      std::make_tuple("basename.0", "basename.0#0",
+                                      "basename.0#1", "basename.0#2"),
+                      std::make_tuple("helloworld.1.1", "helloworld.1.1#0",
+                                      "helloworld.1.1#1", "helloworld.1.1#2"),
+                      std::make_tuple("helloworld#1", "helloworld#1#0",
+                                      "helloworld#1#1", "helloworld#1#2")));
+
+int main(int argc, char **argv)
+{
+    int result;
+    ::testing::InitGoogleTest(&argc, argv);
+
+    result = RUN_ALL_TESTS();
+
+    return result;
+}
