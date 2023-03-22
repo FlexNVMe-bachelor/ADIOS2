dbdbd44358ec6ae5b7a85bcabaa2c78075a6733a
Albert Rise Nielsen <albert@risenielsen.dk>
2023-03-22 14:19:05 +0100

    Add class for creating and managing loopback devices for test

---
+++ testing/adios2/transports/disk/Loopback.h
@@ -0,0 +1,36 @@
+#ifndef LOOPBACK_DEVICE_H
+#define LOOPBACK_DEVICE_H
+
+#include <string>
+
+namespace Disk
+{
+class LoopbackDevice
+{
+public:
+    explicit LoopbackDevice(std::string backingFilePath);
+    explicit LoopbackDevice();
+
+    auto Create() -> std::string;
+    auto GetDeviceUrl() -> std::string;
+
+private:
+    int m_LdctlFileDescriptor = -1;
+    int m_LdFileDescriptor = -1;
+    int m_BackingFileDescriptor = -1;
+
+    long m_DeviceNr = -1;
+
+    std::string m_BackingFileName;
+    std::string m_LoopName;
+
+    void GetCtlFileDescriptor();
+    void GetDeviceNr();
+    void CreateLoopName();
+    void OpenDevice();
+    void OpenBackingFile();
+    void LinkLoopbackAndFile();
+};
+} // namespace Utils
+
+#endif
