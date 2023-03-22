183ba3755715647c870f01a737573224de56cc97
Albert Rise Nielsen <albert@risenielsen.dk>
2023-03-22 16:48:46 +0100

    Create temporary test with new test method

--- testing/adios2/transports/TestFileFlexNVMeOpen.cpp
+++ testing/adios2/transports/TestFileFlexNVMeOpen.cpp
@@ -2,28 +2,41 @@
 
 #include "adios2/helper/adiosCommDummy.h"
 #include "adios2/toolkit/transport/file/FileFlexNVMe.h"
-#include "disk/loopback.h"
+#include "disk/BackingFile.h"
+#include "disk/Loopback.h"
+#include "disk/mkfs.h"
 #include <flexalloc_mm.h>
+#include <iostream>
 
 class OpenTestSuite : public ::testing::Test
 {
 protected:
     Disk::LoopbackDevice device;
+    Disk::BackingFile backingFile;
 
     OpenTestSuite()
     {
-        Disk::LoopbackDevice dev("/tmp/testloop.img");
+        Disk::BackingFile bf(4096, 64);
+        backingFile = bf;
+        backingFile.Create();
+
+        Disk::LoopbackDevice dev(backingFile.GetPath());
         device = dev;
         device.Create();
+
+        mkfs(device.GetDeviceUrl(), 32);
     }
 };
 
 TEST_F(OpenTestSuite, CanOpenTest)
 {
     adios2::transport::FileFlexNVMe e(adios2::helper::CommDummy());
-    e.deviceUrl = const_cast<char *>(device.GetDeviceUrl().c_str());
+    char *deviceUrl = const_cast<char *>(device.GetDeviceUrl().c_str());
+    // e.SetDevice(deviceUrl);
 
-    e.Open("helloworld", adios2::Mode::Write);
+    // std::cout << "flexnvme deviceUrl: " << e.deviceUrl << "\n";
+
+    // e.Open(deviceUrl, adios2::Mode::Write);
 }
 
 int main(int argc, char **argv)
