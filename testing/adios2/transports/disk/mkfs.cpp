8ca3cbe68ad8e42c119a21dd95644b8e1661686d
Albert Rise Nielsen <albert@risenielsen.dk>
2023-03-22 16:00:12 +0100

    Removed debuggin statemens and added exceptions to mkfs.cpp

--- testing/adios2/transports/disk/mkfs.cpp
+++ testing/adios2/transports/disk/mkfs.cpp
@@ -2,6 +2,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
 void mkfs(std::string deviceUrl) { mkfs(deviceUrl, 0, 0); }
 
@@ -28,14 +30,10 @@ void mkfs(std::string deviceUrl, size_t blockCountPrSlab, size_t poolCount)
     command += " " + deviceUrl;
     command += " 2>&1";
 
-    std::cout << "mkfs.flexalloc deviceUrl: " << deviceUrl << "\n";
-    std::cout << "mkfs.flexalloc comand: " << command << "\n";
-
     FILE *proc = popen(command.c_str(), "r");
     if (!proc)
     {
-        std::cerr << "Failed to execute command\n";
-        std::exit(1);
+        throw std::runtime_error("Failed to execute command: " + command);
     }
 
     char buffer[128];
@@ -50,5 +48,10 @@ void mkfs(std::string deviceUrl, size_t blockCountPrSlab, size_t poolCount)
         }
     }
 
-    std::cout << "mkfs.flexalloc output: " << result << "\n";
+    int exitCode = pclose(proc) / 256;
+    if (exitCode != 0)
+    {
+        throw std::runtime_error(
+            "mkfs.flexalloc did not exit with code 0, output:\n" + result);
+    }
 }
