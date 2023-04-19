6bc7cc536e426981e83bdb9d35c8d2f33d01e23c
Adrian Borup <adrian@borup.com>
2023-04-19 15:03:03 +0200

    Add test for overwriting chunks partially

--- testing/adios2/transports/TestFileFlexNVMeReadWrite.cpp
+++ testing/adios2/transports/TestFileFlexNVMeReadWrite.cpp
@@ -171,6 +171,44 @@ TEST_F(ReadWriteTestSuite, CanReadChunksWithOffsetAndSubsizeTest)
     ASSERT_EQ(data.substr(readOffset, readSize), readData);
 }
 
+TEST_F(ReadWriteTestSuite, CanOverwriteChunksPartiallyTest)
+{
+    adios2::transport::FileFlexNVMe transport(adios2::helper::CommDummy());
+    transport.SetParameters(GetParams());
+
+    transport.Open("helloworld", adios2::Mode::Write);
+
+    Rng rng;
+    const size_t MIN_NUM_CHUNKS = 1, MAX_NUM_CHUNKS = 32;
+
+    size_t bufferSize = rng.RandRange(MIN_NUM_CHUNKS * m_blockSize,
+                                      MAX_NUM_CHUNKS * m_blockSize);
+
+    std::string origData = rng.RandString(bufferSize - 1);
+
+    transport.Write(origData.c_str(), bufferSize, 0);
+
+    size_t writeOffset = rng.RandRange(1, bufferSize - 1);
+    size_t writeSize = rng.RandRange(1, bufferSize - writeOffset);
+    std::string newData = rng.RandString(writeSize - 1);
+
+    transport.Write(newData.c_str(), writeSize, writeOffset);
+
+    char *readBuffer = (char *)malloc(bufferSize);
+    transport.Read(readBuffer, bufferSize, 0);
+    std::string readData(readBuffer);
+    free(readBuffer);
+
+    ASSERT_NE(origData, newData);
+    ASSERT_NE(origData, readData);
+    ASSERT_NE(newData, readData);
+
+    ASSERT_EQ(origData.substr(0, writeOffset), readData.substr(0, writeOffset));
+    ASSERT_EQ(newData, readData.substr(writeOffset, writeSize));
+    ASSERT_EQ(origData.substr(writeOffset + writeSize),
+              readData.substr(writeOffset + writeSize));
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
