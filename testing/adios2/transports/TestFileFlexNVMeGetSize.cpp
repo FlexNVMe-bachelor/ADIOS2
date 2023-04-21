edee29dd9563ceb39613c3373549f5bc10ce6a08
Adrian Borup <adrian@borup.com>
2023-04-21 11:43:21 +0200

    Add test for single-byte chunk

--- testing/adios2/transports/TestFileFlexNVMeGetSize.cpp
+++ testing/adios2/transports/TestFileFlexNVMeGetSize.cpp
@@ -81,6 +81,25 @@ TEST_F(GetSizeTestSuite, CanGetSizeOfFullChunksTest)
     ASSERT_EQ(dataSize, transport.GetSize());
 }
 
+TEST_F(GetSizeTestSuite, CanGetSizeOfSingleByteChunkTest)
+{
+    adios2::transport::FileFlexNVMe transport(adios2::helper::CommDummy());
+    transport.SetParameters(GetParams());
+
+    transport.Open("helloworld", adios2::Mode::Write);
+
+    Rng rng;
+    const size_t MIN_NUM_CHUNKS = 1, MAX_NUM_CHUNKS = 16;
+    size_t numChunks = rng.RandRange(MIN_NUM_CHUNKS, MAX_NUM_CHUNKS);
+    size_t dataSize = numChunks * m_blockSize + 1;
+
+    std::string data = rng.RandString(dataSize - 1);
+
+    transport.Write(data.c_str(), dataSize, 0);
+
+    ASSERT_EQ(dataSize, transport.GetSize());
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
