52fa7bdd56c069daa4255eef61d7428e60172876
Adrian Borup <adrian@borup.com>
2023-04-21 10:25:53 +0200

    Add broken append test

--- testing/adios2/transports/TestFileFlexNVMeReadWrite.cpp
+++ testing/adios2/transports/TestFileFlexNVMeReadWrite.cpp
@@ -236,6 +236,43 @@ TEST_F(ReadWriteTestSuite, CanReadManyTimesTest)
     }
 }
 
+TEST_F(ReadWriteTestSuite, CanAppendDataTest)
+{
+    adios2::transport::FileFlexNVMe transport(adios2::helper::CommDummy());
+    transport.SetParameters(GetParams());
+
+    transport.Open("helloworld", adios2::Mode::Write);
+
+    Rng rng;
+    const size_t MIN_NUM_CHUNKS = 1, MAX_NUM_CHUNKS = 16;
+
+    size_t origBufferSize = rng.RandRange(MIN_NUM_CHUNKS * m_blockSize,
+                                          MAX_NUM_CHUNKS * m_blockSize);
+    size_t appendBufferSize = rng.RandRange(MIN_NUM_CHUNKS * m_blockSize,
+                                            MAX_NUM_CHUNKS * m_blockSize);
+    size_t totalSize = origBufferSize + appendBufferSize;
+
+    std::string data = rng.RandString(totalSize - 1);
+
+    transport.Write(data.c_str(), origBufferSize, 0);
+
+    char *readBuffer = (char *)malloc(totalSize);
+
+    ASSERT_THROW(transport.Read(readBuffer, totalSize, 0), std::range_error);
+
+    ASSERT_NO_THROW(transport.Write(data.substr(origBufferSize).c_str(),
+                                    appendBufferSize, origBufferSize));
+
+    ASSERT_NO_THROW(transport.Read(readBuffer, totalSize, 0));
+
+    std::string readData(readBuffer);
+
+    free(readBuffer);
+
+    ASSERT_EQ(data.length(), readData.length());
+    ASSERT_EQ(data, readData);
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
