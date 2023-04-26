f9b3afa8610ec61cd876d56f35f7516aa897ae71
Adrian Borup <adrian@borup.com>
2023-04-26 11:36:05 +0200

    Add another broken test

--- testing/adios2/transports/TestFileFlexNVMeMatchingFilePOSIX.cpp
+++ testing/adios2/transports/TestFileFlexNVMeMatchingFilePOSIX.cpp
@@ -13,6 +13,7 @@
 #include <memory>
 #include <random>
 #include <string>
+#include <utility>
 
 class FileFlexNVMeFilePOSIXMatchTestSuite : public Disk::DiskTestClass
 {
@@ -53,7 +54,76 @@ TEST_F(FileFlexNVMeFilePOSIXMatchTestSuite, SingleWriteAndReadYieldsTheSame)
     std::string posixResult = performTestOnTransport(posix);
     std::string flexnvmeResult = performTestOnTransport(flexnvme);
 
-    ASSERT_EQ(flexnvmeResult, posixResult);
+    ASSERT_EQ(std::make_pair(posixResult.length(), posixResult),
+              std::make_pair(flexnvmeResult.length(), flexnvmeResult));
+}
+
+TEST_F(FileFlexNVMeFilePOSIXMatchTestSuite,
+       MultipleWriteAndSingleReadYieldsTheSame)
+{
+    Rng rng;
+
+    adios2::transport::FilePOSIX posix(adios2::helper::CommDummy());
+    adios2::transport::FileFlexNVMe flexnvme(adios2::helper::CommDummy());
+    flexnvme.SetParameters(GetParams());
+
+    std::string name = rng.RandString(24, 'a', 'z');
+
+    const size_t NUM_WRITES = 4;
+    size_t bufferSize = rng.RandRange(NUM_WRITES * 3, 3 * m_blockSize);
+    std::string writeData = rng.RandString(bufferSize - 1);
+
+    std::array<size_t, NUM_WRITES> batchSizes{};
+    size_t sizeLeft = bufferSize;
+
+    for (int i = 0; i < NUM_WRITES; i++)
+    {
+        if (i == NUM_WRITES - 1)
+        {
+            batchSizes[i] = sizeLeft;
+            break;
+        }
+
+        size_t subSize = rng.RandRange(1, sizeLeft - 2);
+        batchSizes[i] = subSize;
+        sizeLeft -= subSize;
+    }
+
+    auto performTestOnTransport = [&](adios2::Transport &transport) {
+        transport.Open(name, adios2::Mode::Write);
+
+        int startIndex = 0;
+        for (auto batchSize : batchSizes)
+        {
+            std::string batchWriteData =
+                writeData.substr(startIndex, batchSize);
+            transport.Write(batchWriteData.c_str(), batchSize,
+                            adios2::MaxSizeT);
+            startIndex += batchSize;
+        }
+
+        transport.Close();
+
+        char *readBuffer = (char *)malloc(bufferSize);
+
+        transport.Open(name, adios2::Mode::Read);
+        size_t size = transport.GetSize();
+        transport.Read(readBuffer, size, adios2::MaxSizeT);
+        transport.Close();
+
+        std::string readData(readBuffer);
+        free(readBuffer);
+
+        return std::make_pair(size, readData);
+    };
+
+    auto posixResult = performTestOnTransport(posix);
+    auto flexnvmeResult = performTestOnTransport(flexnvme);
+
+    ASSERT_EQ(posixResult.first, flexnvmeResult.first);
+    ASSERT_EQ(
+        std::make_pair(posixResult.second.length(), posixResult.second),
+        std::make_pair(flexnvmeResult.second.length(), flexnvmeResult.second));
 }
 
 int main(int argc, char **argv)
